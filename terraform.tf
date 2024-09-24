fbb7c33cf251ae2e8fdfcc67cc7ec2ddd946aa14
Dylan Seppings <dylanseppings@gmail.com>
2024-09-24 16:47:13 +0200

    Moved to Remote storage for state file

--- terraform.tf
+++ terraform.tf
@@ -1,5 +1,10 @@
 terraform {
-  backend "local" {}
+    backend "azurerm" {
+    resource_group_name  =  var.backend_rg_name 
+    storage_account_name  = var.backend_storage_account_name 
+    container_name        = var.backend_container_name 
+    key                   = var.backend_key 
+  }
 
   required_providers {
     azurerm = {
